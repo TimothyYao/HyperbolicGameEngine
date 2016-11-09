bbca5b14b45020ffec09fc2aa94716323b377bd3
timothyyao <timmyyao555@gmail.com>
2016-11-08 23:53:33 -0500

    Initial Commit

---
+++ hyperbolic game engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hyperbolic game engine
+//
+//  Created by Timothy Yao on 11/8/16.
+//  Copyright © 2016 Timothy Yao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
